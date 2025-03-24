7abccb6e860bbd1e97740ee916920c021c000d8a
Ebrahim Elngar <infinity.technology2024@gmail.com>
2025-03-24 10:11:25 +0200

    first commit

---
+++ src/Pages/Signup/Signup.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+
+function Signup() {
+    return (
+        <form>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-6">
+                        <div className="divImg d-none d-xl-block" style={{ height: "775px" }}>
+                            <img
+                                src="../assets/photoMarket.png"
+                                alt="photoMarket in Login"
+                                style={{ height: "100%", width: "100%" }}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="col-xl-6 col-12 d-flex justify-content-center" style={{ marginTop: "102px" }}>
+                        <div className="loginForm">
+                            <div className="loginTitle">
+                                <h2 className='fw-bold' style={{ fontSize: "40px" }}>تسجيل الدخول</h2>
+                            </div>
+
+                            <div>
+                                <label
+                                    className='d-block'
+                                    htmlFor="email"
+                                    style={{
+                                        fontWeight: "500",
+                                        fontSize: "15px",
+                                        marginBottom: "14px",
+                                        marginTop: "46px"
+                                    }}
+                                >
+                                    البريد الإلكتروني
+                                </label>
+
+                                <input
+                                    style={{
+                                        width: "349px",
+                                        height: "36px",
+                                        borderRadius: "5px",
+                                        border: "1px solid #D9D9D9",
+                                        padding: "10px"
+                                    }}
+                                    className='custom-placeholder'
+                                    type="email"
+                                    placeholder="أدخل عنوان بريدك الالكترونى"
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    className='d-block'
+                                    htmlFor="password"
+                                    style={{
+                                        fontWeight: "500",
+                                        fontSize: "15px",
+                                        marginBottom: "14px",
+                                        marginTop: "29px"
+                                    }}
+                                >
+                                    الرقم السرى
+                                </label>
+
+                                <input
+                                    style={{
+                                        width: "349px",
+                                        height: "36px",
+                                        borderRadius: "5px",
+                                        border: "1px solid #D9D9D9",
+                                        padding: "10px"
+                                    }}
+                                    className='custom-placeholder'
+                                    type="password"
+                                    placeholder="أدخل الرقم السرى الخاص بك"
+                                />
+                            </div>
+
+                            <div
+                                style={{
+                                    marginTop: "79px"
+                                }}
+                            >
+                                <button
+                                    style={{
+                                        width: "107px",
+                                        height: "36px",
+                                        border: "none",
+                                        borderRadius: "5px",
+                                        backgroundColor: "#125A7A",
+                                        color: "#FFFFFF",
+                                        fontWeight: "500",
+                                        fontSize: "12px"
+                                    }}>تسجيل جديد</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </form>
+    )
+}
+
+export default Signup
